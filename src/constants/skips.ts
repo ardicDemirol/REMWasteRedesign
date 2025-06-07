bfb8fb2e53c1646099cc3334a3798aabad7daa32
Ardıç Demirol <ardc.dmrol@gmail.com>
2025-06-08 00:54:12 +0300

    Update skips.ts

--- src/constants/skips.ts
+++ src/constants/skips.ts
@@ -1,6 +1,6 @@
 export async function getSkips(): Promise<any[]> {
   const response = await fetch(
-    "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
+    "https://cors-anywhere.herokuapp.com/https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
   );
   if (!response.ok) throw new Error("Failed to fetch skips");
   return response.json();
